9e83c2b2981d4f29880b2162190b347bd8e28e45
snek152 <kakanisnehil@gmail.com>
2021-10-23 22:24:30 -0700

    hashrouter change

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom'
 import ReactDOM from 'react-dom'
 import Calculator from "./Calculator"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./index.css"
-import { HashRouter } from 'react-router-dom';
 import Scientific from './Scientific'
 
 ReactDOM.render(
