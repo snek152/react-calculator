e15b48fca64d69e4f722904d980654e9107cbd1e
snek152 <kakanisnehil@gmail.com>
2021-10-24 11:03:47 -0700

    changing hashrouter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 import ReactDOM from 'react-dom'
 import Calculator from "./Calculator"
 import "bootstrap/dist/css/bootstrap.min.css"
@@ -7,9 +7,9 @@ import "./index.css"
 import Scientific from './Scientific'
 
 ReactDOM.render(
-  <HashRouter basename="/">
+  <BrowserRouter basename="/">
     <Route exact path="/" component={Calculator} />
     <Route path="/scientific" component={Scientific} />
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById('container')
 )
